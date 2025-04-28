c462904574c46c2ca82dc306f8323321cf9b2472
DiegoBustos16 <diegombustos16@gmail.com>
2025-04-28 14:22:37 -0300

    feat: enhance unit tests for Doctor and HospitalDoctor services with Mockito for improved mocking and dependency injection

--- hospital-microservice/src/test/java/com/visor/hospital_microservice/service/DoctorServiceTest.java
+++ hospital-microservice/src/test/java/com/visor/hospital_microservice/service/DoctorServiceTest.java
@@ -10,6 +10,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -17,11 +22,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class DoctorServiceTest {
 
-    private DoctorService doctorService;
+    @Mock
     private DoctorClient doctorClient;
+
+    @Mock
     private HospitalDoctorRepository hospitalDoctorRepository;
+
+    @InjectMocks
+    private DoctorService doctorService;
+
 
     @BeforeEach
     void setUp() {
--- hospital-microservice/src/test/java/com/visor/hospital_microservice/service/HospitalDoctorServiceTest.java
+++ hospital-microservice/src/test/java/com/visor/hospital_microservice/service/HospitalDoctorServiceTest.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +23,7 @@ import static org.mockito.Mockito.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.openMocks;
 
+@ExtendWith(MockitoExtension.class)
 class HospitalDoctorServiceTest {
 
     @Mock
