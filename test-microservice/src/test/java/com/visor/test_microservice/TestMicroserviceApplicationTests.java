5b4954c3302f899b591e520be340f98bc2c999da
DiegoBustos16 <diegombustos16@gmail.com>
2025-05-06 15:50:21 -0300

    refactor: remove unused contextLoads test method from TestMicroserviceApplicationTests

--- test-microservice/src/test/java/com/visor/test_microservice/TestMicroserviceApplicationTests.java
+++ test-microservice/src/test/java/com/visor/test_microservice/TestMicroserviceApplicationTests.java
@@ -1,11 +1,8 @@
 package com.visor.test_microservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestMicroserviceApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+
 }
